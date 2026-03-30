b8602dab860bfbcea9306c19c5a2e523621bc419
kritsadaskt <k.sangkhatad@hotmail.com>
2026-03-30 08:47:13 +0700

    Switch box FGF , AFF in frontpage

--- src/modules/components/landing/CreatorTools.tsx
+++ src/modules/components/landing/CreatorTools.tsx
@@ -10,16 +10,16 @@ export function CreatorTools() {
         <p className="text-center text-neutral-50 font-light text-base lg:text-xl">รับโอกาสในการเข้าถึงเครื่องมือสร้างรายได้<br className="block lg:hidden" />จากคอนเท้นของแอสเซทไวส์</p>
         <div className="h-10"></div>
         <div className="flex flex-col md:flex-row justify-center gap-7 max-w-6xl mx-auto">
-          <div className="fgf bg-gradient-to-br min-h-[250px] from-white to-orange-100 w-full md:w-1/2 rounded-lg p-7 shadow-lg relative overflow-hidden pb-[200px] lg:pb-0">
-            <h4 className="text-3xl font-medium mb-2 text-primary">FRIEND GET FRIENDS</h4>
-            <p className="text-black w-2/3">ชวนเพื่อนมาเป็นเพื่อนบ้าน ในโครงการบ้านและคอนโดมิเนียมของ ASSETWISE รับค่าแนะนำสูงสุด 500,000 บ.*</p>
-            <Image src={fgf_icon} alt="FRIEND GET FRIENDS" width={500} height={500} className="absolute -bottom-[30%] left-0 lg:-bottom-[35%] lg:-right-[15%] lg:left-auto w-full lg:w-[320px] h-auto" />
-          </div>
           <div className="aff bg-gradient-to-br min-h-[250px] from-white to-orange-100 w-full md:w-1/2 rounded-lg p-7 shadow-lg relative overflow-hidden pb-[200px] lg:pb-0">
             <h4 className="text-3xl font-medium mb-2 text-primary">AFFILIATE PROGRAM</h4>
             <p className="text-black w-full lg:w-2/3">เลือกโปรโมท 36 โครงการจาก ASSETWISE ผ่านโซเชียลมีเดียเพื่อรับค่าคอมมิชชั่นง่าย ๆ
             เพียงแชร์ลิงก์หรือโพสต์คอนเทนต์ เมื่อมีการจองรับผลตอบแทนสูงสุดถึง 500,000 บาท*</p>
             <Image src={aff_icon} alt="AFFILIATE PROGRAM" width={500} height={500} className="absolute -bottom-[30%] left-0 lg:-bottom-[35%] lg:-right-[15%] lg:left-auto w-full lg:w-[320px] h-auto" />
+          </div>
+          <div className="fgf bg-gradient-to-br min-h-[250px] from-white to-orange-100 w-full md:w-1/2 rounded-lg p-7 shadow-lg relative overflow-hidden pb-[200px] lg:pb-0">
+            <h4 className="text-3xl font-medium mb-2 text-primary">FRIEND GET FRIENDS</h4>
+            <p className="text-black w-2/3">ชวนเพื่อนมาเป็นเพื่อนบ้าน ในโครงการบ้านและคอนโดมิเนียมของ ASSETWISE รับค่าแนะนำสูงสุด 500,000 บ.*</p>
+            <Image src={fgf_icon} alt="FRIEND GET FRIENDS" width={500} height={500} className="absolute -bottom-[30%] left-0 lg:-bottom-[35%] lg:-right-[15%] lg:left-auto w-full lg:w-[320px] h-auto" />
           </div>
         </div>
       </div>
