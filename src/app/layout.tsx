35da2012a077140cda420a0cabf3e4e9b6db1a0f
kritsadaskt <k.sangkhatad@hotmail.com>
2026-04-01 13:52:14 +0700

    move up GTM tag to <head>

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,14 +18,16 @@ const GTM_ID = 'GTM-MM872QW';
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="th">
-      <body>
-        <Script id="gtm" strategy="afterInteractive">
+      <head>
+      <Script id="gtm" strategy="afterInteractive">
           {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
 })(window,document,'script','dataLayer','${GTM_ID}');`}
         </Script>
+      </head>
+      <body>
         <noscript>
           <iframe
             src={`https://www.googletagmanager.com/ns.html?id=${GTM_ID}`}
