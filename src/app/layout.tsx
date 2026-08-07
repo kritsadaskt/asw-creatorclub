1d54113ceb760b7a6ab7d7d89c5d90c03ecc0055
kritsadaskt <k.sangkhatad@hotmail.com>
2026-08-07 11:16:01 +0700

    fix: move CDP snipped from GTM to <head/>

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -59,6 +59,7 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
 })(window,document,'script','dataLayer','${GTM_ID}');`}
         </Script>
+        <script src="https://cdn.sable.asia/tracking-672b8dd9c38c6d2ad2a8bbdd.js" data-cfasync="false"></script>
       </head>
       <body>
         <noscript>
