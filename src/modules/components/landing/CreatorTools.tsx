8f8c94845a0e04f208223f295ea1399056cf318f
kritsadaskt <k.sangkhatad@hotmail.com>
2026-09-18 09:52:26 +0700

    fix: adjust css for front page

--- src/modules/components/landing/CreatorTools.tsx
+++ src/modules/components/landing/CreatorTools.tsx
@@ -4,6 +4,8 @@ import { FormEvent, useEffect, useState } from 'react';
 import { toast } from 'sonner';
 import fgf_icon from '@/assets/fgf-img.webp';
 import aff_icon from '@/assets/aff-img.webp';
+import aff_icon_mobile from '@/assets/aff-img-m.webp';
+import fgf_icon_mobile from '@/assets/fgf-img-m.webp';
 import Image from 'next/image';
 import Link from 'next/link';
 import { useSession } from '@/modules/context/SessionContext';
@@ -87,23 +89,25 @@ export function CreatorTools() {
     <section id="creator_tools" className="relative bg-primary text-white pt-10 pb-10">
       <div className="container mx-auto px-6">
         <h3 className="text-3xl lg:text-5xl font-medium text-center mb-2">เป็นครีเอเตอร์กับเราวันนี้</h3>
-        <p className="text-center text-neutral-50 font-light text-base lg:text-xl">รับโอกาสในการเข้าถึงเครื่องมือสร้างรายได้<br className="block lg:hidden" />จากคอนเทนต์</p>
+        <p className="text-center text-neutral-50 font-light text-base lg:text-xl">รับโอกาสในการเข้าถึงเครื่องมือสร้างรายได้<br className="block md:hidden" />จากคอนเทนต์</p>
         <div className="h-10"></div>
         <div className="flex flex-col md:flex-row justify-center gap-7 max-w-6xl mx-auto">
-          <div className="aff bg-gradient-to-br min-h-[250px] from-white to-orange-100 w-full md:w-1/2 rounded-lg p-7 shadow-lg relative overflow-hidden pb-[200px] lg:pb-0">
+          <div className="aff bg-gradient-to-br min-h-[250px] from-white to-orange-100 w-full md:w-1/2 rounded-lg p-7 pb-0 xl:pb-7 shadow-lg relative overflow-hidden">
             <h4 className="text-3xl font-medium mb-2 text-primary">AFFILIATE PROGRAM</h4>
-            <p className="text-black w-full lg:w-2/3">เลือกโปรโมท 36 โครงการจาก ASSETWISE ผ่าน<br className='hidden lg:block' />โซเชียลมีเดียเพื่อรับค่าคอมมิชชันง่าย ๆ
+            <p className="text-black w-full lg:w-2/3">เลือกโปรโมท 36 โครงการจาก ASSETWISE ผ่าน<br className='hidden xl:block' />โซเชียลมีเดียเพื่อรับค่าคอมมิชชันง่าย ๆ
             เพียงแชร์ลิงก์หรือโพสต์คอนเทนต์ เมื่อมีการจองรับผลตอบแทนสูงสุดถึง 500,000 บาท*</p>
-            <Image src={aff_icon} alt="AFFILIATE PROGRAM" width={500} height={500} className="absolute -bottom-[30%] left-0 lg:-bottom-[35%] lg:-right-[15%] lg:left-auto w-full lg:w-[320px] h-auto" />
+            <Image src={aff_icon} alt="AFFILIATE PROGRAM" width={500} height={500} className="hidden lg:block absolute -bottom-[30%] left-0 lg:-bottom-[30%] lg:-right-[13%] lg:left-auto w-full md:w-[250px] xl:w-[280px] h-auto xl:-scale-x-100" />
             <div className="h-5"></div>
             <Link href="/affiliate" className="bg-accent hover:bg-accent/90 text-accent-foreground px-4 py-2 rounded-md">ดูเพิ่มเติม</Link>
+            <Image src={aff_icon_mobile} alt="AFFILIATE PROGRAM" width={500} height={500} className="block lg:hidden w-4/5 h-auto float-right" />
           </div>
-          <div className="fgf bg-gradient-to-br min-h-[250px] from-white to-orange-100 w-full md:w-1/2 rounded-lg p-7 shadow-lg relative overflow-hidden pb-[200px] lg:pb-0">
+          <div className="fgf bg-gradient-to-br min-h-[250px] from-white to-orange-100 w-full md:w-1/2 rounded-lg p-7 pb-0 shadow-lg relative overflow-hidden">
             <h4 className="text-3xl font-medium mb-2 text-primary">FRIEND GET FRIENDS</h4>
             <p className="text-black w-2/3">ชวนเพื่อนมาเป็นเพื่อนบ้าน ในโครงการบ้านและคอนโดมิเนียมของ ASSETWISE รับค่าแนะนำสูงสุด 500,000 บาท*</p>
-            <Image src={fgf_icon} alt="FRIEND GET FRIENDS" width={500} height={500} className="absolute -bottom-[30%] left-0 lg:-bottom-[35%] lg:-right-[15%] lg:left-auto w-full lg:w-[320px] h-auto" />
+            <Image src={fgf_icon} alt="FRIEND GET FRIENDS" width={500} height={500} className="hidden lg:block absolute -bottom-[30%] left-0 lg:-bottom-[35%] lg:-right-[15%] lg:left-auto w-full md:w-[250px] xl:w-[320px] h-auto" />
             <div className="h-5"></div>
             <Link href="/friendgetfriends" className="bg-accent hover:bg-accent/90 text-accent-foreground px-4 py-2 rounded-md relative">ดูเพิ่มเติม</Link>
+            <Image src={fgf_icon_mobile} alt="FRIEND GET FRIENDS" width={500} height={500} className="block lg:hidden w-[183px] h-auto float-right" />
           </div>
         </div>
       </div>
